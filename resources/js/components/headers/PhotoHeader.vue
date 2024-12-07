28b8bebf096248f399cd11626313ca46abd91b67
Gendra13 <52973190+Gendra13@users.noreply.github.com>
2024-12-07 12:36:28 +0100

    Fullsize photo view

--- resources/js/components/headers/PhotoHeader.vue
+++ resources/js/components/headers/PhotoHeader.vue
@@ -1,7 +1,7 @@
 <template>
 	<header
 		id="lychee_toolbar_container"
-		class="absolute top-0 left-0 w-full flex-none z-10 bg-gradient-to-b from-black h-14"
+		class="absolute top-0 left-0 w-full flex-none z-10 from-black h-14"
 		:class="is_full_screen || is_slideshow_active ? 'opacity-0 hover:opacity-100' : 'opacity-100 h-14'"
 	>
 		<Toolbar class="w-full bg-transparent border-0">
